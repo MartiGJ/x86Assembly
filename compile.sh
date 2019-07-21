c869993262113751c2b2054c8e944165bef11442
root <root@kali.vm>
2019-07-21 19:15:43 +0200

    Fixed compile.sh

--- compile.sh
+++ compile.sh
@@ -6,12 +6,18 @@ set -e
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo "[!] \"${last_command}\" command failed with exit code $?."' EXIT
+function finish {
+    exit_status=$?
+    if [ $exit_status -ne 0 ]; then
+        echo "[!] [${last_command}] command failed with exit code $exit_status."
+    else
+        echo '[+] Done!'
+    fi
+}
+trap finish EXIT
 
 echo '[+] Assembling with Nasm...'
 nasm -f elf32 -o $1.o $1.asm
 
 echo '[+] Linking ...'
 ld -m elf_i386 -o $1 $1.o
-
-echo '[+] Done!'
