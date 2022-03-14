c26abf2424ee60d4be0695e03b26465e3550e7e9
MartiGJ <35295849+MartiGJ@users.noreply.github.com>
2022-03-14 14:14:43 +0100

    Updated gen_shellcode to Python3

--- Tools/gen_shellcode.py
+++ Tools/gen_shellcode.py
@@ -1,18 +1,25 @@
-#!/bin/python
+#!/usr/bin/env python3
 import os
 import sys
-if len(sys.argv) != 2 :
-    print "Usage: "+sys.argv[0]+" <file.asm>."
-    sys.exit()
 
-cmd = "nasm -fbin -o shellcode.bin " +sys.argv[1]
-exit_status = os.system(cmd)
-if exit_status != 0:
-    print "[!] Error executing nasm."
-    sys.exit()
+def main():
+    if len(sys.argv) != 2 :
+        print(f"Usage: {sys.argv[0]} <file.asm>.")
+        sys.exit()
 
-with open("shellcode.bin") as f: 
-    code = f.read()
-    print '\\x'+'\\x'.join(x.encode('hex') for x in code)
+    cmd = f"nasm -fbin -o shellcode.bin {sys.argv[1]}"
+    exit_status = os.system(cmd)
+    if exit_status != 0:
+        print("[!] Error executing nasm.")
+        sys.exit()
 
-os.remove("shellcode.bin")
+    print("[+] Shellcode:")
+    with open("shellcode.bin","rb") as f:
+        code = f.read()
+        print(''.join(f"\\x{x:02X}" for x in code))
+
+    if os.path.exists("shellcode.bin"):
+        os.remove("shellcode.bin")
+
+if __name__ == '__main__':
+    main()
